07c1e0a2d129b5bf7410655af9c7ab541ef8a9c4
Samer-Ismael <samerismaeelbat2@hotmail.com>
2024-03-14 11:18:15 +0100

    Fixing City model

--- src/main/java/org/koffa/model/City.java
+++ src/main/java/org/koffa/model/City.java
@@ -2,9 +2,15 @@ package org.koffa.model;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class City {
     private long id;
     private String cityName;
