7cbc4a5a1703a4388c6f37375eeba0969fd19b3a
Samer-Ismael <samerismaeelbat2@hotmail.com>
2024-03-14 11:17:55 +0100

    Making City model

--- src/main/java/org/koffa/model/City.java
+++ src/main/java/org/koffa/model/City.java
@@ -1,4 +1,13 @@
 package org.koffa.model;
 
+import java.util.List;
+
+import lombok.Data;
+
+@Data
 public class City {
+    private long id;
+    private String cityName;
+    private List<Employee> employees;
+
 }
